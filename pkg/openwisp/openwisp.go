fa9b1881f7a55e15dc0dae8de643cc37caf06cad
vivek seshadri <vseshadri@bastion.localdomain>
2021-03-24 14:09:44 -0400

    new packages to support openwisp ipam

---
+++ pkg/openwisp/openwisp.go
@@ -0,0 +1,417 @@
+package openwisp
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+	"net/http"
+	"net/url"
+	"os"
+        "io/ioutil"
+	"strings"
+
+	//"github.com/dougbtv/whereabouts/pkg/allocate"
+	"github.com/dougbtv/whereabouts/pkg/logging"
+	"github.com/dougbtv/whereabouts/pkg/types"
+	"github.com/dougbtv/whereabouts/pkg/podData"
+	"encoding/json"
+)
+
+var (
+	// RequestTimeout defines how long the context timesout in
+	RequestTimeout = 5 * time.Second
+
+	// DatastoreRetries defines how many retries are attempted when updating the Pool
+	DatastoreRetries = 20
+)
+
+var ipamConf types.IPAMConfig
+
+type IPAMPlug struct {
+        Type       string        `json:"ipamtype,omitempty"`
+        Url        string        `json:"ipamurl,omitempty"`
+        Username   string        `json:"ipamuser,omitempty"`
+        Password   string        `json:"ipampwd,omitempty"`
+}
+
+//decoding responses from openwisp api's
+// get the token after authentication
+type IPAMToken struct {
+	Token      string        `json:"token"`
+}
+
+type SubnetDescription struct {
+	Defaultgw   string       `json:"gw"`
+}
+
+//get the list of subnets to make sure the requested one is present and also extract default GW from description field
+type SubnetInfo struct {
+	Id      string        `json:"id"`
+	Name	string        `json:"name"`
+	Subnet  string        `json:"subnet"`
+	Desc    string        `json:"description"`
+	IP      string        `json:"ip_address",omitempty`
+}
+
+type SubnetList struct {
+	SList    []SubnetInfo    `json:"results"`
+}
+
+//json format to reserve a new IP in a subnet
+type SubnetIPCreate struct {
+	IP        string         `json:"ip_address"`
+	Desc      string         `json:"description"`
+	SubnetID  string         `json:"subnet"`
+}
+
+// IPPool is the interface that represents an manageable pool of allocated IPs
+type IPPool interface {
+	Allocations() []types.IPReservation
+	Update(ctx context.Context, reservations []types.IPReservation) error
+}
+
+// Store is the interface that wraps the basic IP Allocation methods on the underlying storage backend
+type Store interface {
+	GetIPPool(ctx context.Context, ipRange string) (IPPool, error)
+	Status(ctx context.Context) error
+	Close() error
+}
+
+func getCredentials(ipam IPAMPlug) (string, error) {
+    urlstring := "http://" + ipam.Url + "/api/v1/user/token/"
+    resp, err := http.PostForm(urlstring,
+	url.Values{"username": {ipam.Username}, "password": {ipam.Password}})
+    defer resp.Body.Close()
+    if resp.StatusCode == 200 {
+        body, _ := ioutil.ReadAll(resp.Body)
+        //logging.Debugf("Body is %s and %d",body,resp.StatusCode)
+	var tok IPAMToken
+        if err := json.Unmarshal(body, &tok); err != nil {
+		return "", err
+	}
+        return tok.Token, err
+    } 
+    logging.Errorf("Response code is %s", resp.StatusCode)
+    return "", err
+}
+
+func allocateIP(token string, ipam IPAMPlug, pod string , pool string) (net.IPNet, string, error) {
+	var newip net.IPNet
+	// First we will get the list of subnet to make sure we have the subnet of interest and also get teh Defalt GW for that subnet ID
+        urlstring := "http://" + ipam.Url + "/api/v1/subnet/"
+	client := &http.Client{}
+        req, _ := http.NewRequest("GET", urlstring, nil)
+	btoken := "Bearer " + token
+	req.Header.Set("Authorization", btoken)
+        resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	var subnetList SubnetList
+	if resp.StatusCode == 200 {
+        	body, err := ioutil.ReadAll(resp.Body)
+        	//logging.Debugf("Body is %s and %d",body,resp.StatusCode)
+        	//var tok IPAMToken
+        	if err := json.Unmarshal(body, &subnetList); err != nil {
+			logging.Errorf("json decoding failure of %s with error %s",body,err)
+                	return newip,"", err
+        	}
+		logging.Debugf("decoded data is %v",subnetList.SList)
+		// we will go through the list to find the subnet of interest and extract its id and gw info
+		
+		id := ""
+		gw := ""
+		sn := ""
+		for _,subnets := range  subnetList.SList {
+			if subnets.Name == pool {
+				id = subnets.Id
+				var sgw SubnetDescription
+				json.Unmarshal([]byte(subnets.Desc),&sgw)
+				gw = sgw.Defaultgw
+				sn = subnets.Subnet
+				break
+			}
+		}
+		logging.Debugf("id for pool %s is %s with def gw %s and subnet %s",pool,id,gw,sn)
+
+		// check if a entry already exists for th podRef
+		urlstring := "http://" + ipam.Url + "/api/v1/subnet/" + id + "/ip-address"
+                client := &http.Client{}
+                req, _ := http.NewRequest("GET", urlstring, nil)
+                btoken := "Bearer " + token
+                req.Header.Set("Authorization", btoken)
+                resp, _ := client.Do(req)
+                defer resp.Body.Close()
+                var ipList SubnetList
+                if resp.StatusCode != 200 {
+                        return newip, "",fmt.Errorf("error")
+                }
+                body, _ = ioutil.ReadAll(resp.Body)
+                if err := json.Unmarshal(body, &ipList); err != nil {
+                        logging.Errorf("json decoding failure of %s with error %s",body,err)
+                        return newip,"", err
+                }
+                logging.Debugf("response is %v",ipList.SList)
+                ipId := ""
+		ipAddr := ""
+                for _, ipBlock := range ipList.SList {
+                        if ipBlock.Desc == pod {
+                                ipId = ipBlock.Id
+				ipAddr = ipBlock.IP
+                                break
+                        }
+                }
+                if ipId != "" {
+                        logging.Debugf(" IP %s already allocated for pod %s",ipAddr, pod)
+			subnet_det := strings.Split(sn, "/")
+                        ipNet := ipAddr + "/" + subnet_det[1]
+                //      newip.IP = []byte(ip)
+                //      newip.Mask = []byte(subnet_det[1])
+                        _,newip1,_ := net.ParseCIDR(ipNet)
+                        newip.IP = net.ParseIP(ipAddr)
+                        newip.Mask = newip1.Mask
+
+                        return newip, gw ,nil 
+                }
+		//end of check
+
+		// get the next free ip in subnet
+		allocated := 0
+		retries := DatastoreRetries
+		for allocated < 1 &&  retries > 0 {
+               		urlstring := "http://" + ipam.Url + "/api/v1/subnet/" + id + "/get-next-available-ip/"
+        		client := &http.Client{}
+        		req, _ := http.NewRequest("GET", urlstring, nil)
+        		btoken := "Bearer " + token
+        		req.Header.Set("Authorization", btoken)
+			resp, _ := client.Do(req)
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				return newip, "", fmt.Errorf("error response %d",resp.StatusCode)
+			}
+			body, _ := ioutil.ReadAll(resp.Body)
+			ip := strings.Trim(string(body), "\"")
+			logging.Debugf("Next free IP available in pool %s is %s",pool, ip)
+			var ipDet SubnetIPCreate
+			ipDet.IP = ip
+			ipDet.Desc = pod
+			ipDet.SubnetID = id
+			ipDet_json,_ := json.Marshal(ipDet)
+			req_body := strings.NewReader(string(ipDet_json))
+			logging.Debugf("encoded data is %s",ipDet_json)
+			urlstring = "http://" + ipam.Url + "/api/v1/subnet/" + id + "/ip-address/"
+			client1 := &http.Client{}
+                        req, _ = http.NewRequest("POST", urlstring, req_body)
+                        btoken = "Bearer " + token
+                        req.Header.Set("Authorization", btoken)
+			req.Header.Set("Content-Type", "application/json")
+			logging.Debugf("Request is %s",req)
+                        resp, err = client1.Do(req)
+			defer resp.Body.Close()
+                        if resp.StatusCode != 201 {
+				logging.Debugf("Got response %s and %s",resp,err)
+                                return newip, "", fmt.Errorf("error response %d",resp.StatusCode)
+                        } else {
+			    allocated = 1
+			    retries--
+			}
+			subnet_det := strings.Split(sn, "/")
+			ipNet := ip + "/" + subnet_det[1]
+		//	newip.IP = []byte(ip)
+		//	newip.Mask = []byte(subnet_det[1])
+		        _,newip1,_ := net.ParseCIDR(ipNet) 
+                        newip.IP = net.ParseIP(ip)
+			newip.Mask = newip1.Mask
+			logging.Debugf("Allocation of IP %s for pod %s successfull",ip,pod)
+			return newip, gw, nil
+		}
+        	return newip,gw, err
+        }
+        return newip, "",fmt.Errorf("error")
+}
+
+func deallocateIP(token string, ipam IPAMPlug, pod string , pool string) (net.IPNet, string, error) {
+	var newip net.IPNet
+	// First we will get the list of subnet to make sure we have the subnet of interest and also get teh Defalt GW for that subnet ID
+        urlstring := "http://" + ipam.Url + "/api/v1/subnet/"
+	client := &http.Client{}
+        req, _ := http.NewRequest("GET", urlstring, nil)
+	btoken := "Bearer " + token
+	req.Header.Set("Authorization", btoken)
+        resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	var subnetList SubnetList
+	if resp.StatusCode == 200 {
+        	body, _ := ioutil.ReadAll(resp.Body)
+        	//logging.Debugf("Body is %s and %d",body,resp.StatusCode)
+        	//var tok IPAMToken
+        	if err := json.Unmarshal(body, &subnetList); err != nil {
+			logging.Errorf("json decoding failure of %s with error %s",body,err)
+                	return newip,"", err
+        	}
+		logging.Debugf("decoded data is %v",subnetList.SList)
+		// we will go through the list to find the subnet of interest and extract its id and gw info
+		
+		id := ""
+		gw := ""
+		sn := ""
+		for _,subnets := range  subnetList.SList {
+			if subnets.Name == pool {
+				id = subnets.Id
+				var sgw SubnetDescription
+				json.Unmarshal([]byte(subnets.Desc),&sgw)
+				gw = sgw.Defaultgw
+				sn = subnets.Subnet
+				break
+			}
+		}
+		logging.Debugf("id for pool %s is %s with def gw %s and subnet %s",pool,id,gw,sn)
+		// get the next free ip in subnet
+		urlstring := "http://" + ipam.Url + "/api/v1/subnet/" + id + "/ip-address"
+        	client := &http.Client{}
+        	req, _ := http.NewRequest("GET", urlstring, nil)
+        	btoken := "Bearer " + token
+        	req.Header.Set("Authorization", btoken)
+        	resp, _ := client.Do(req)
+		defer resp.Body.Close()
+        	var ipList SubnetList
+		if resp.StatusCode != 200 {
+			return newip, "",fmt.Errorf("error")
+		}
+		body, _ = ioutil.ReadAll(resp.Body)
+		if err := json.Unmarshal(body, &ipList); err != nil {
+                        logging.Errorf("json decoding failure of %s with error %s",body,err)
+                        return newip,"", err
+                }
+		logging.Debugf("response is %v",ipList.SList)
+		ipId := ""
+		for _, ipBlock := range ipList.SList {
+			if ipBlock.Desc == pod {
+				ipId = ipBlock.Id
+				break
+			}
+		}
+		if ipId == "" {
+			logging.Errorf("No IP allocated for pod %s",pod)
+			return newip,"", fmt.Errorf("error")
+		}
+		logging.Debugf("Trying to deallocate IP with id %s",ipId)
+		urlstring = "http://" + ipam.Url + "/api/v1/ip-address/" + ipId + "/" 
+		client1 := &http.Client{}
+                req, _ = http.NewRequest("DELETE", urlstring, nil)
+                btoken = "Bearer " + token
+                req.Header.Set("Authorization", btoken)
+		resp1, _ := client1.Do(req)
+		defer resp1.Body.Close()
+                if resp1.StatusCode != 204 {
+			logging.Errorf("Deallocating ip with id %s failed request is %s and response is %s",ipId,req,resp1)
+                        return newip, "",fmt.Errorf("error")
+                }
+		logging.Debugf("Successfully deallocated IP for id %s",ipId)
+		return newip, "success", nil
+        }
+        return newip, "",fmt.Errorf("error")
+}
+
+
+// IPManagement manages ip allocation and deallocation from a storage perspective
+func IPManagement(mode int, ipamConf types.IPAMConfig, containerID string, podRef string) (net.IPNet, string, error) {
+
+	logging.Debugf("IPManagement -- mode: %v / host: %v / containerID: %v / podRef: %v", mode, ipamConf.EtcdHost, containerID, podRef)
+
+	var newip net.IPNet
+	ipamProv := IPAMPlug{}
+	// Skip invalid modes
+	/*
+	switch mode {
+	case types.Allocate, types.Deallocate:
+	default:
+		return newip, fmt.Errorf("Got an unknown mode passed to IPManagement: %v", mode)
+	}
+	*/
+	/*
+
+// IPManagement manages ip allocation and deallocation from a storage perspective
+func IPManagement(mode int, ipamConf types.IPAMConfig, containerID string, podRef string) (net.IPNet, string, error) {
+
+	logging.Debugf("IPManagement -- mode: %v / host: %v / containerID: %v / podRef: %v", mode, ipamConf.EtcdHost, containerID, podRef)
+
+	var newip net.IPNet
+	ipamProv := IPAMPlug{}
+	// Skip invalid modes
+	/*
+	switch mode {
+	case types.Allocate, types.Deallocate:
+	default:
+		return newip, fmt.Errorf("Got an unknown mode passed to IPManagement: %v", mode)
+	}
+	*/
+	/*
+	var ipam Store
+	var pool IPPool
+	var err error
+	switch ipamConf.Datastore {
+	case types.DatastoreETCD:
+		ipam, err = NewETCDIPAM(ipamConf)
+	case types.DatastoreKubernetes:
+		ipam, err = NewKubernetesIPAM(containerID, ipamConf)
+	}
+	if err != nil {
+		logging.Errorf("IPAM %s client initialization error: %v", ipamConf.Datastore, err)
+		return newip, fmt.Errorf("IPAM %s client initialization error: %v", ipamConf.Datastore, err)
+	}
+	defer ipam.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), RequestTimeout)
+	defer cancel()
+        
+	// Check our connectivity first
+	if err := ipam.Status(ctx); err != nil {
+		logging.Errorf("IPAM connectivity error: %v", err)
+		return newip, err
+	}
+	*/
+
+        jsonFile, err := os.Open("/etc/cni/net.d/whereabouts.d/whereabouts-ipam.conf")
+
+        if err != nil {
+            logging.Debugf("1")	
+            return newip, "",fmt.Errorf("Error opening flat configuration file %s", err)
+        }
+
+        defer jsonFile.Close()
+
+        jsonBytes, err := ioutil.ReadAll(jsonFile)
+        if err != nil {
+            logging.Debugf("2")
+            return newip, "",fmt.Errorf("LoadIPAMConfig Flatfile - ioutil.ReadAll error: %s", err)
+        }
+
+        if err := json.Unmarshal(jsonBytes, &ipamProv); err != nil {
+            logging.Debugf("3")
+            return newip, "",fmt.Errorf("LoadIPAMConfig Flatfile - JSON Parsing Error: %s / bytes: %s", err, jsonBytes)
+        } 
+
+	logging.Debugf("IpamProv is %s and decoded is %v",jsonBytes, ipamProv)
+	credential, err := getCredentials(ipamProv)
+        logging.Debugf("The credential token is %s",credential)
+	podRef = podData.GetPodAnnotation(ipamConf, podRef)
+	logging.Debugf("podref after annotation search is %s",podRef)
+	switch mode {
+	case types.Allocate:
+            return allocateIP(credential, ipamProv, podRef, "test")            	
+	case types.Deallocate:
+            return deallocateIP(credential, ipamProv, podRef, "test")
+	default:
+	    logging.Errorf("Unknown mode of operation")
+	}
+	return newip, "dummy", err
+}
+
+/*func main() {
+	ipamConf.Kubernetes.KubeConfigPath = "/etc/cni/net.d/whereabouts.d/whereabouts.kubeconfig"
+	fmt.Println("Hello, playground")
+	//ip1,gw, _ := IPManagement(0,ipamConf,"dummy","default/test-5d68d4fdf6-pdmbr")
+	//logging.Debugf("At main after allocate is %s with gw %s",string(ip1.IP),gw)
+        IPManagement(1,ipamConf,"dummy","default/test-5d68d4fdf6-pdmbr")
+}*/
+
---
+++ pkg/podData/podData.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
+package podData
+
+import (
+	//"context"
+	"flag"
+	"fmt"
+	//"path/filepath"
+	"strings"
+	//"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"github.com/dougbtv/whereabouts/pkg/types"
+	"github.com/dougbtv/whereabouts/pkg/logging"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+func GetPodAnnotation(ipamConf types.IPAMConfig, podRef string) (string) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", ipamConf.Kubernetes.KubeConfigPath, "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", ipamConf.Kubernetes.KubeConfigPath, "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		logging.Errorf("error in buildconfig")
+		return podRef
+	}
+
+	// create the clientset
+	logging.Debugf("newforconfig")
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logging.Errorf("error in newforconfig")
+		return podRef
+	}
+	for {
+		pods, err1 := clientset.CoreV1().Pods("").List( metav1.ListOptions{})
+		if err1 != nil {
+			logging.Errorf("error in reading pod data %s", err1)
+			return podRef
+		}
+		logging.Debugf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		podRefVal := strings.Split(podRef, "/")
+		namespace := podRefVal[0]
+		pod := podRefVal[1]
+		poddata, err := clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			return podRef
+		} else {
+			logging.Debugf("Found pod %s in namespace %s with data %s\n", pod, namespace,poddata.ObjectMeta.Annotations)
+			for annoName,annoVal := range poddata.ObjectMeta.Annotations {
+				if annoName == "podname" {
+					podRef = annoVal
+					logging.Debugf("found pod with annotation name podname with value %s",annoVal)
+				}
+			}
+		}
+		return podRef
+
+	}
+}
